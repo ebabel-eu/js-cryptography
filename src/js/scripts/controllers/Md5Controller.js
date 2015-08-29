d1d53df9bf8fa615c44a53c07e180dc0d170a23b
nadjibamar <hello@ebabel.eu>
2015-08-29 12:01:25 +0200

    trying to solve a bug

--- src/js/scripts/controllers/Md5Controller.js
+++ src/js/scripts/controllers/Md5Controller.js
@@ -3,10 +3,20 @@ angular
     .controller('Md5Controller', [
 
         function Md5Controller () {
+            var controller = this;
+
             this.email = {
                 text: null,
                 hash: null
             };
+
+            this.setHash = function () {
+                return 'test@test.com';
+            };
+
+            this.sayHello = function () {
+                return controller.email.text = 'Hello';
+            }
         }
 
     ]
